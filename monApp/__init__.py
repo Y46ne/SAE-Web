7c6e4a807a229e41bc453288a11dc57d3b5d2708
clement-vgc <clement.vignon-chaudey@etu.univ-orleans.fr>
2025-11-17 17:37:29 +0100

    création pages login, créer compte et index

--- monApp/__init__.py
+++ monApp/__init__.py
@@ -1,4 +1,2 @@
 from .app import app, db 
-import monApp.views
 import monApp.commands
-import monApp.database
--- monApp/app.py
+++ monApp/app.py
@@ -1,6 +1,5 @@
 from flask import Flask  
 from flask_sqlalchemy import SQLAlchemy
-from flask_bootstrap5 import Bootstrap
 from flask_login import LoginManager
 
 app = Flask(__name__, 
@@ -11,8 +10,8 @@ app.config.from_object('config')
 db = SQLAlchemy()
 db.init_app(app)
 
-Bootstrap(app)
-
 login_manager = LoginManager(app)
 login_manager.login_view = "login"
 app.config['SQLALCHEMY_ECHO'] = True
+
+from monApp import views
